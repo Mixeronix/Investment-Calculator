d1c32d865cf3f0e0cc1bc13b6df0975bc1b7f8a6
Bartosz Wiaderek <bartoszwiaderek@gmail.com>
2023-05-23 01:14:45 +0200

    Button hover effect

--- components/button.tsx
+++ components/button.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import { Open_Sans } from "next/font/google";
+import { useEffect } from "react";
 
 const openSansFont = Open_Sans({
 	weight: "700",
@@ -6,14 +9,42 @@ const openSansFont = Open_Sans({
 });
 
 export default function Button(props: { text: string }) {
+	useEffect(() => {
+		const button = document.querySelector("button");
+
+		const handleMouseMove = (event: { clientX: number; clientY: number }) => {
+			const rect: any = button?.getBoundingClientRect();
+			const ripple: any = document.getElementById("ripple");
+
+			const x = event.clientX - rect.x;
+			const y = event.clientY - rect.y;
+
+			if (x > 0 && x < rect.width) ripple.style.left = `${x}px`;
+			if (y > 0 && y < rect.height) ripple.style.top = `${y}px`;
+		};
+
+		window.addEventListener("mousemove", handleMouseMove);
+
+		return () => {
+			window.removeEventListener("mousemove", handleMouseMove);
+		};
+	}, []);
 	return (
 		<button
+			id={"clickableButton"}
 			className={
-				"xl:bottom-18 relative rounded-xl bg-violet-600 px-10 py-3 text-center text-xl leading-snug tracking-wide text-white lg:absolute lg:bottom-14 lg:px-14 lg:py-4 lg:text-2xl 2xl:bottom-32 2xl:px-20 2xl:py-5 2xl:text-3xl " +
+				"xl:bottom-18 group relative overflow-hidden rounded-xl bg-violet-600 px-10 py-3 text-center text-xl leading-snug tracking-wide lg:absolute lg:bottom-14 lg:px-14 lg:py-4 lg:text-2xl 2xl:bottom-32 2xl:px-20 2xl:py-5 2xl:text-3xl  " +
 				openSansFont.className
 			}
 		>
-			{props.text}
+			<div
+				id="ripple"
+				className="pointer-events-none absolute aspect-square h-auto w-0 -translate-x-1/2 -translate-y-1/2 rounded-full bg-white transition-all duration-500 group-hover:w-2full"
+			></div>
+			<div className="absolute left-1/2 top-1/2 z-10 -translate-x-1/2 -translate-y-1/2 text-violet-600 opacity-0 transition-all group-hover:opacity-100">
+				{props.text}
+			</div>
+			<div className="text-white">{props.text}</div>
 		</button>
 	);
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
 	content: ["./pages/**/*.{js,ts,jsx,tsx,mdx}", "./components/**/*.{js,ts,jsx,tsx,mdx}", "./app/**/*.{js,ts,jsx,tsx,mdx}"],
 	theme: {
-		extend: {},
+		extend: {
+			width: {
+				"2full": "200%",
+			},
+		},
 	},
 	plugins: [require("prettier-plugin-tailwindcss")],
 };
